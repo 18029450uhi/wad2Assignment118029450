42290e7e1af919f0101d9a1e141d452308a39ed5
SefatNur <71414775+18029450uhi@users.noreply.github.com>
2024-05-20 15:22:19 +0100

    alevelcomputing modified file uploaded

---
+++ wad2Assignment18029450/alevelcomputing/alevelcomputing/client/src/components/IWTSC.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useRef} from 'react';
+
+function IWTSC() {
+
+    return (
+        <div>
+            <iframe
+                src="http://localhost:3000"
+                title="IWTSC"
+                style={{width: '100%', height: '100vh'}}
+            />
+        </div>
+    );
+}
+
+export default IWTSC;
+
